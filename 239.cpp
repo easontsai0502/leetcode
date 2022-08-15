2d3641359f7e7df670b40a629f53094c8a88ef51
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-15 11:16:24 +0800

    a

--- 239.cpp
+++ 239.cpp
@@ -1,6 +1,6 @@
 /*
 [Q]https://leetcode.com/problems/sliding-window-maximum/
-[]
+[TLE]
 */
 class Solution {
     public:
@@ -14,13 +14,13 @@ class Solution {
             using namespace std;
             vector<int> ans;
             priority_queue<pii,vector<pii>,cmp> pq;
-            for(int i=0;i<nums.size(),i++){
+            for(int i=0;i<nums.size();i++){
                 pq.push({nums[i],i});
                 if(i+1>=k){
-                    while(i-(pq.back()).second<k){
+                    while(i-(pq.top()).second<k){
                         pq.pop();
                     }
-                    ans.push_back((pq.back()).first);
+                    ans.push_back((pq.top()).first);
                 }
             }
             return ans;
