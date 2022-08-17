fe8cb5b50fb75c36739d60a78052fb448f0c5b5e
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-17 15:38:54 +0800

    704

---
+++ 704.cpp
@@ -0,0 +1,12 @@
+/*
+[Q]https://leetcode.com/problems/binary-search/
+[]
+*/
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        #define ulli unsigned long long int
+        #define lli long long int
+        ulli lef=0;rig=nums.size()-1;
+    }
+};
