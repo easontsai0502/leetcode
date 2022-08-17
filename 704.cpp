c42e8f51cff9bf50c0ad76fada88fa5983e22804
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-08-17 15:51:53 +0800

    a

--- 704.cpp
+++ 704.cpp
@@ -3,10 +3,33 @@
 []
 */
 class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        #define ulli unsigned long long int
-        #define lli long long int
-        ulli lef=0;rig=nums.size()-1;
-    }
+	public:
+		int search(vector<int>& nums, int target) {
+			#define ulli unsigned long long int
+			#define lli long long int
+			ulli lef=0;rig=nums.size()-1,ans;
+			while(true){
+				if(nums[(rig+lef)/2]>target) rig=(rig+lef)/2;
+				else if(nums[(rig+lef)/2]<target) lef=(rig+lef)/2;
+				else{
+					ans=(rig+lef)/2;
+					break;
+				}
+				if(rig-lef<=2){
+					if(nums[lef]==target){
+						ans=lef;
+						break;
+					}else if(nums[lef+1]==target){
+						ans=lef+1;
+					}else if(nums[rig]==target){
+						ans=rig;
+						break;
+					}else{
+						ans=-1;
+						break;
+					}
+				}
+			}
+			return ans;
+		}
 };
