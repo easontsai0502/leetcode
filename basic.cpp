797b14113f50c9623a07145d0ed8ea3c65600535
ET01 <66681962+easontsai0502@users.noreply.github.com>
2022-11-04 19:08:53 +0800

    a

---
+++ basic.cpp
@@ -0,0 +1,22 @@
+/*
+[Q]
+[]
+*/
+/*define*/
+#define what_the_fuck cin.tie(0);cout.tie(0);ios::sync_with_stdio(false)
+#define ULLI unsigned long long int
+#define LLI long long int
+#define INT int
+#define UINT unsigned INT
+#define PII pair<INT,INT>
+#define PUIUI pair<UINT,UINT>
+#define endl "\n"
+#define wassomething() empty()==false
+/*struct*/
+struct super_pair{
+	INT x1=0;
+	INT x2=0;
+	INT x3=0;
+	INT x4=0;
+	INT x5=0;
+};
